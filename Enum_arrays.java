18f82e6500b26f2a12d1e12caa4a5172f1d7b1d6
Nisarga0110 <nisarga7419@gmail.com>
2026-01-29 12:37:56 +0530

    Add files via upload

---
+++ Enum_arrays.java
@@ -0,0 +1,29 @@
+package Nisarga;
+
+enum Day
+{
+MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+public class Enum_arrays
+{
+public static void main(String[] args) {
+ 
+Day[] allDays=Day.values();
+ 
+System.out.println("All Days of the Week:");
+for(Day d: allDays)
+{
+	System.out.println(d);
+	//Day[] WorkingDays=new Day[5];
+	//WorkingDays[0]= Day.MONDAY;
+}
+	
+	
+	
+	}
+ 
+}
+ 
+ 
+
+	
